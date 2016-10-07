bd25866fb31a0e43e23cd46976b489af1a2c2075
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2016-10-07 12:48:16 -0500

    Implemented goToLocation for corridor

--- src/locations/CorridorLocation.cpp
+++ src/locations/CorridorLocation.cpp
@@ -25,6 +25,41 @@ CorridorLocation::CorridorLocation(const CorridorLocation& cl)
 }
 
 bool CorridorLocation::goToLocation(actionlib::SimpleActionClient<bwi_kr_execution::ExecutePlanAction>& client) {
+    bwi_kr_execution::ExecutePlanGoal goal;
 
-    client.sendGoal();
+    bwi_kr_execution::AspRule rule;
+    bwi_kr_execution::AspFluent fluent;
+    fluent.name = "not at";
+    fluent.variables.push_back(aspLocation);
+    rule.body.push_back(fluent);
+    goal.aspGoal.push_back(rule);
+
+    client.sendGoal(goal);
+
+    client.waitForResult(ros::Duration(300, 0));
+
+    // If goal is not done in the timeout limit (5s), cancel goal and return failed
+    if (!client.getState().isDone()) {
+        ROS_ERROR_STREAM("Canceling goal to location: " << aspLocation);
+        client.cancelGoal();
+        client.waitForResult(ros::Duration(1, 0));
+        return false;
+    }
+    if (client.getState() == actionlib::SimpleClientGoalState::ABORTED) {
+        ROS_ERROR_STREAM("Aborted goal to location " << aspLocation);
+        return false;
+    }
+    else if (client.getState() == actionlib::SimpleClientGoalState::PREEMPTED) {
+        ROS_ERROR_STREAM("Preempted goal to location " << aspLocation);
+        return false;
+    }
+
+    else if (client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+        ROS_ERROR_STREAM("Succeeded goal to location " << aspLocation);
+        return true;
+    }
+    else {
+        ROS_ERROR_STREAM("Terminated goal to location " << aspLocation);
+        return false;
+    }
 }
