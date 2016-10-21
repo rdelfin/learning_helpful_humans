790eff9a6c165c4664394c8cb6ec7543febb03ca
Ricardo Delfin <ricardo.delfin.garcia@gmail.com>
2016-10-21 10:51:22 -0500

    Added FLTK dependency to image_asker_node.

--- src/image_asker_node.cpp
+++ src/image_asker_node.cpp
@@ -1,14 +1,17 @@
 #include <ros/ros.h>
 #include <bwi_msgs/ImageQuestion.h>
-#include <bwi_msgs/QuestionDialog.h>
 
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <thread>
 #include <mutex>
 
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Box.H>
+
 ros::ServiceServer server;
-ros::ServiceClient questionClient;
+//ros::ServiceClient questionClient;
 std::string windowName = "Display Window";
 std::string question = "How would you describe the image on the right?\n(type nothing in box and press enter to make me leave)";
 
@@ -24,7 +27,7 @@ int main(int argc, char* argv[]) {
     ros::NodeHandle nh;
     
     server = nh.advertiseService("ask_location", askQuestion);
-    questionClient = nh.serviceClient<bwi_msgs::QuestionDialog>("/question_dialog");
+    //questionClient = nh.serviceClient<bwi_msgs::QuestionDialog>("/question_dialog");
 
     ROS_INFO("Creating window...");
     cv::namedWindow(windowName, cv::WINDOW_AUTOSIZE);
@@ -43,42 +46,19 @@ int main(int argc, char* argv[]) {
 
 bool askQuestion(bwi_msgs::ImageQuestionRequest& req, bwi_msgs::ImageQuestionResponse& res) {
     ROS_INFO("Question request made");
-    //m.lock();
-    //cv::namedWindow(windowName, cv::WINDOW_AUTOSIZE);
     img = cv_bridge::toCvCopy(req.image);
-    cv::imshow(windowName, img->image);
-	cv::waitKey(3);
-    //m.unlock();
-
-    bwi_msgs::QuestionDialogRequest questionReq;
-    bwi_msgs::QuestionDialogResponse questionRes;
-    
-    questionReq.message = question;
-    questionReq.timeout = 30.0;
-    questionReq.type = bwi_msgs::QuestionDialogRequest::TEXT_QUESTION;
-    questionClient.call(questionReq, questionRes);
-
-    //m.lock();
-    //cv::destroyWindow(windowName);
-    //m.unlock();
-    
-    if(questionRes.text != "")
-        res.answers.push_back(questionRes.text);
     
     return true;
 }
 
 void imgThread() {
-    /*ROS_INFO("Second thread!");
-    ros::Rate r(10);
-    while(ros::ok()) {
-        if(img != nullptr) {
-            m.lock();
-            cv::imshow(windowName, img->image);
-            cv::waitKey(0);
-            img = nullptr;
-            m.unlock();
-        }
-        r.sleep();
-    }*/
+    Fl_Window *window = new Fl_Window(300,180);
+    Fl_Box *box = new Fl_Box(20,40,260,100,"Hello, World!");
+    box->box(FL_UP_BOX);
+    box->labelsize(36);
+    box->labelfont(FL_BOLD+FL_ITALIC);
+    box->labeltype(FL_SHADOW_LABEL);
+    window->end();
+    window->show(0, {});
+    Fl::run();
 }
