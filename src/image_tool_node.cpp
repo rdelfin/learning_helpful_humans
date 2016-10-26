b8c98134cb5cb7e450ec276c149ff44132ef67e2
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2016-10-26 14:21:00 -0500

    Added image_tool_node target

--- src/image_tool_node.cpp
+++ src/image_tool_node.cpp
@@ -2,3 +2,12 @@
 // Created by rdelfin on 10/26/16.
 //
 
+#include <ros/ros.h>
+
+int main(int argc, char* argv[]) {
+    ros::init(argc, argv, "image_tool_node");
+
+    ros::spin();
+
+    return 0;
+}
