57c9d82a0d538cca2339cb534c598477dded5710
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2016-11-08 15:22:24 -0600

    Implemented GetFieldValue

--- include/learning_helpful_humans/request/GetFieldValue.h
+++ include/learning_helpful_humans/request/GetFieldValue.h
@@ -5,9 +5,21 @@
 #ifndef PROJECT_GETFIELDVALUE_HPP
 #define PROJECT_GETFIELDVALUE_HPP
 
+#include <string>
+#include <json/json.hpp>
+
+using json = nlohmann::json;
 
 class GetFieldValue {
+public:
+    GetFieldValue(std::string path);
 
+    json perform();
+
+    ~GetFieldValue();
+private:
+    std::string path;
+     std::string server;
 };
 
 
--- src/request/GetFieldValue.cpp
+++ src/request/GetFieldValue.cpp
@@ -2,4 +2,51 @@
 // Created by rdelfin on 11/8/16.
 //
 
-#include "GetFieldValue.hpp"
+#include <learning_helpful_humans/request/GetFieldValue.h>
+#include <curlpp/cURLpp.hpp>
+#include <curlpp/Easy.hpp>
+#include <curlpp/Options.hpp>
+
+#include <sstream>
+
+#include <json/json.hpp>
+
+using json = nlohmann::json;
+
+
+GetFieldValue::GetFieldValue(std::string path)
+        : path(path), server("robotimages-dacc9.firebaseio.com") {
+
+}
+
+json GetFieldValue::perform() {
+    std::stringstream urlStream;
+    urlStream << server << "/" << path;
+    std::string url = urlStream.str();
+
+    try {
+        curlpp::Cleanup myCleanup;
+        curlpp::Easy req;
+
+        // POST field
+        req.setOpt(new curlpp::options::Url(url));
+
+        req.perform();
+
+        std::stringstream result;
+        result << req;
+
+        return json::parse(result.str());
+
+    } catch(curlpp::RuntimeError & e) {
+        std::cerr << e.what() << std::endl;
+        return json();
+    } catch(curlpp::LogicError & e) {
+        std::cerr << e.what() << std::endl;
+        return json();
+    }
+}
+
+GetFieldValue::~GetFieldValue() {
+
+}
