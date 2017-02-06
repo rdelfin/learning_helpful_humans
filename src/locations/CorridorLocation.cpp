4f6dabfea62d8dafbbbe8c14d8dc16f92a2c97ce
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2017-02-06 15:32:10 -0600

    minor changes

--- src/locations/CorridorLocation.cpp
+++ src/locations/CorridorLocation.cpp
@@ -73,13 +73,11 @@ bool CorridorLocation::goToCorridor(actionlib::SimpleActionClient<bwi_kr_executi
     ROS_INFO_STREAM("Going to " << aspLocation);
     client.sendGoal(goal);
 
-    client.waitForResult(ros::Duration(300, 0));
+    bool timed_out = client.waitForResult(ros::Duration(200, 0));
 
-    // If goal is not done in the timeout limit (5s), cancel goal and return failed
-    if (!client.getState().isDone()) {
+    // If goal is not done in the timeout limit, cancel goal and return failed
+    if (timed_out) {
         ROS_WARN_STREAM("Canceling goal to location: " << aspLocation);
-        client.cancelGoal();
-        client.waitForResult(ros::Duration(1, 0));
         return false;
     }
     if (client.getState() == actionlib::SimpleClientGoalState::ABORTED) {
