b97cc88640c5a910ae421d35bc0e1413ac6762b9
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2018-02-07 13:34:53 -0600

    Modified location_selector_node to get next action from SARSA agent

--- learning_helpful_humans/src/location_selector_node.cpp
+++ learning_helpful_humans/src/location_selector_node.cpp
@@ -8,7 +8,14 @@
 
 #include <ros/ros.h>
 
-#include "bwi_kr_execution/ExecutePlanAction.h"
+#include <bwi_kr_execution/ExecutePlanAction.h>
+#include <bwi_kr_execution/CurrentStateQuery.h>
+
+#include <bwi_msgs/ActionUpdate.h>
+#include <bwi_msgs/ImageQuestion.h>
+#include <bwi_msgs/LocationAction.h>
+#include <bwi_msgs/NextLocation.h>
+#include <bwi_msgs/Trigger.h>
 
 #include <actionlib/client/simple_action_client.h>
 
@@ -18,29 +25,29 @@
 #include <learning_helpful_humans/locations/OfficeLocation.hpp>
 #include <learning_helpful_humans/request/GetFieldValue.h>
 
-#include <ros/package.h>
-
 #include <json/json.hpp>
-#include <bwi_msgs/ImageQuestion.h>
-#include <bwi_msgs/NextLocation.h>
-#include <bwi_msgs/Trigger.h>
 
-#include <bwi_kr_execution/CurrentStateQuery.h>
+#include <ctime>
+#include <random>
+#include <unordered_map>
+
 
 using json = nlohmann::json;
 
-std::vector<AskLocation*> locations;
+std::unordered_map<std::string, AskLocation*> locations;
 actionlib::SimpleActionClient<bwi_kr_execution::ExecutePlanAction>* planClient;
 actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction>* moveBaseClient;
 ros::ServiceClient stopClient;
 ros::ServiceClient currentStateClient;
-
-
-int locIdx = 0;
+ros::ServiceClient getSarsaActionClient;
 
 bool nextQuestionCallback(bwi_msgs::NextLocationRequest& req, bwi_msgs::NextLocationResponse& res);
 
 void loadLocations();
+
+bool location_known = false;
+std::string current_location_id = "";
+std::default_random_engine generator;
 
 int main(int argc, char* argv[]) {
     ros::init(argc, argv, "location_selector_node");
@@ -53,10 +60,13 @@ int main(int argc, char* argv[]) {
     moveBaseClient = new actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction>("/move_base", true);
     stopClient = nh.serviceClient<bwi_msgs::Trigger>("stop_base");
     currentStateClient = nh.serviceClient<bwi_kr_execution::CurrentStateQuery> ("/current_state_query");
+    getSarsaActionClient = nh.serviceClient<bwi_msgs::LocationAction>("next_sarsa_action");
 
     planClient->waitForServer();
     moveBaseClient->waitForServer();
     stopClient.waitForExistence();
+    currentStateClient.waitForExistence();
+    getSarsaActionClient.waitForExistence();
 
     ros::Rate r(10);
     while(ros::ok()) {
@@ -64,8 +74,8 @@ int main(int argc, char* argv[]) {
         r.sleep();
     }
 
-    for(AskLocation* loc : locations)
-        delete loc;
+    for(std::pair<std::string, AskLocation*> loc : locations)
+        delete loc.second;
 
     return 0;
 }
@@ -102,7 +112,7 @@ void loadLocations() {
             } else
                 continue;
 
-            locations.push_back(loc);
+            locations[it.key()] = loc;
         }
 
         ROS_INFO("DONE");
@@ -111,15 +121,52 @@ void loadLocations() {
     }
 }
 
+int day_time_seconds() {
+    time_t t = time(0);   // get time now
+    struct tm * now = localtime( & t );
+    return now->tm_sec + 60*(now->tm_min + 60*now->tm_hour);
+}
+
+int day_week() {
+    time_t t = time(0);   // get time now
+    struct tm * now = localtime( & t );
+    return now->tm_wday == 0 ? 6 : now->tm_wday - 1;
+}
+
+std::pair<std::string, bool> get_next_action() {
+    bwi_msgs::LocationActionRequest actionRequest;
+    bwi_msgs::LocationActionResponse actionResponse;
+
+    actionRequest.current_location = current_location_id;
+    actionRequest.time.time_seconds = day_time_seconds();
+    actionRequest.time.day_of_week = day_week();
+
+    getSarsaActionClient.call(actionRequest, actionResponse);
+
+    return {actionResponse.location, actionResponse.ask_question};
+}
+
 bool nextQuestionCallback(bwi_msgs::NextLocationRequest& req, bwi_msgs::NextLocationResponse& res) {
-    ROS_INFO_STREAM("Going to \"" << locations[locIdx]->getName()
-              << "\" (" << locations[locIdx]->getAspLocation()
-              << ") of type " << locations[locIdx]->getTypeString());
 
-    res.locationName = locations[locIdx]->getName();
-    res.success = locations[locIdx]->goToLocation(*planClient, *moveBaseClient, stopClient, currentStateClient);
+    // If the location isn't known already, go to a random location and start from there.
+    if(!location_known) {
+        std::uniform_int_distribution<int> dist(0, locations.size() - 1);
+        auto item = locations.begin();
+        std::advance(item, dist(generator));
+        std::string next_id = item->first;
 
-    locIdx++;
-    if(locIdx >= locations.size())
-        locIdx = 0;
+        locations[next_id]->goToLocation(*planClient, *moveBaseClient, stopClient, currentStateClient);
+        current_location_id = next_id;
+        location_known = true;
+    }
+
+    std::pair<std::string, bool> next_action = get_next_action();
+
+    AskLocation* loc = locations[next_action.first];
+    ROS_INFO_STREAM("Going to \"" << locations[next_action.first]->getName()
+              << "\" (" << locations[next_action.first]->getAspLocation()
+              << ") of type " << locations[next_action.first]->getTypeString());
+
+    res.locationName = locations[next_action.first]->getName();
+    res.success = locations[next_action.first]->goToLocation(*planClient, *moveBaseClient, stopClient, currentStateClient);
 }
